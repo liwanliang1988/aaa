607ff4cb81808968f3612558860ea7795c42b3c3
lwl_glb <lwl_glb@126.com>
2019-10-22 22:16:27 +0800

    12

--- src/main/java/aaa/a.java
+++ src/main/java/aaa/a.java
@@ -4,7 +4,7 @@ public class a {
 public static void main(String[] args) {
 	System.out.println("55555");
 	System.out.println("haha");
-	System.out.println("10.2266666");
+	System.out.println("12");
 	
 
 }
