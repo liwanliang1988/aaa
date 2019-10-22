9bca78298a633a5cbc199a935545f53d394c1cd6
lwl_glb <lwl_glb@126.com>
2019-10-22 20:58:17 +0800

    567

---
+++ src/main/java/aaa/a.java
@@ -0,0 +1,12 @@
+package aaa;
+
+public class a {
+public static void main(String[] args) {
+	System.out.println("55555");
+	System.out.println("haha");
+	System.out.println("10.22");
+	System.out.println("第二次啊");
+	System.out.println("第4次");
+
+}
+}
