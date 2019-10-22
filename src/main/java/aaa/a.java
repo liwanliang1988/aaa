7cf37b93351c5a72984a33156eb247bb0ff60017
lwl_glb <lwl_glb@126.com>
2019-10-22 22:08:08 +0800

    4566

--- src/main/java/aaa/a.java
+++ src/main/java/aaa/a.java
@@ -5,8 +5,7 @@ public static void main(String[] args) {
 	System.out.println("55555");
 	System.out.println("haha");
 	System.out.println("10.22");
-	System.out.println("第二次啊");
-	System.out.println("第8次1111");
+	
 
 }
 }
